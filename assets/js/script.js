4c94f94cadfd43fad0ddacd2e8c644ab1a5ff158
TolgaAkaMoon <4160141@RTS-365.dk>
2019-05-29 11:59:25 +0200

    added products section with css, js & html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,45 @@
-window.onscroll = function() {scrollFunction()};
 
+// Change .navbar when i scroll higher than 30px //
+window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.querySelector(".navbar").style.position = "fixed";
     document.querySelector(".navbar").style.top = "0";
   } else {
-    document.querySelector(".navbar").style.position = "unset";
+    document.querySelector(".navbar").style.position = "absolute";
     document.querySelector(".navbar").style.top = "30px";
   }
 }
 
-// Making multiple element with multiple values inside one (let), for Products //
+// Products //
 let results = [
-    {img: "./assets/img/Tee-Shirt.png", desc: "Smart Phone", price: "$649", label: "new"},
+    {img: "./assets/img/Tee-Shirt.png", desc: "GG/Geared Gamers - White Tee-Shirt", price: "$39", label: "hot"},
+    {img: "./assets/img/Tee-Shirt.png", desc: "GG x Nike Therma-FIT Hooded Sweatshirt", price: "$50", label: "new"},
+    {img: "./assets/img/Tee-Shirt.png", desc: "GG/Geared Gamers - White Tee-Shirt", price: "$29", label: "hot"},
+    {img: "./assets/img/Tee-Shirt.png", desc: "GG/Geared Gamers - White Tee-Shirt", price: "$19", label: "sold"},
+    {img: "./assets/img/Tee-Shirt.png", desc: "GG/Geared Gamers - White Tee-Shirt", price: "$29", label: "new"},
+    {img: "./assets/img/Tee-Shirt.png", desc: "GG/Geared Gamers - White Tee-Shirt", price: "$40", label: "hot"},
 ];
 
-// Takes info from the above (let) & adds an HTML for producs, with that info! //
 results.forEach(element => {
-    if(element.label){
-        document.querySelector('.merch').innerHTML +=
+  if(element.label){
+      document.querySelector('.merch').innerHTML +=
     `<figure class="merch__fig">
-        <a class="fig__anker" href="#">
-            <img class="anker__img" src=${element.img}>
-            <figcaption class="anker__desc">${element.desc}</figcaption>
-            <figcaption class="anker__price">${element.price}</figcaption>
-        </a>
-    </figure>`;
-    }
+      <a class="fig__anker" href="#">
+          <img class="anker__img" src=${element.img}>
+          <div class="anker--${element.label}"><span class="hot--${element.label}">${element.label}</span></div>
+          <figcaption class="anker__desc">${element.desc}</figcaption>
+          <figcaption class="anker__price">${element.price}</figcaption>
+      </a>
+  </figure>`;
+  } else {
+      document.querySelector('.merch').innerHTML +=
+   `<figure class="merch__fig">
+       <a class="fig__anker" href="#">
+           <img class="anker__img" src=${element.img}>
+           <figcaption class="anker__desc">${element.desc}</figcaption>
+           <figcaption class="anker__price">${element.price}</figcaption>
+       </a>
+   </figure>`;
+  }
 });
